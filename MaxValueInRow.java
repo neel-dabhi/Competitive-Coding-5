b3ff0037bb1895eb2acc23e30b30414cd45c1bc4
neel-dabhi <yoursdabhineelkanth@gmail.com>
2025-06-16 16:44:03 -0300

    Completed CC 5

---
+++ MaxValueInRow.java
@@ -0,0 +1,48 @@
+// Time Complexity O(n)
+// Space Complexity O(n)
+// Runs on LeetCode YES
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        List<Integer> result = new ArrayList<Integer>();
+
+        if(root == null) return result;
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            int max = Integer.MIN_VALUE;
+            for(int i=0; i< size; i++){
+                TreeNode curr = q.poll();
+                if(curr.val >= max){
+                    max = curr.val;
+                }
+
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right != null){
+                    q.add(curr.right);
+                }
+
+            }
+            result.add(max);
+        }
+        return result;
+    }
+}
